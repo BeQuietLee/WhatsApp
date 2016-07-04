d481f64d36565596993bf58e1daed80cf7c3833f
lei.li <lei.li@dianping.com>
2016-07-04 22:01:01 +0800

    add WeChat

--- app/src/main/java/work/lilei/whatsapp/atlas/AtlasActivity.java
+++ app/src/main/java/work/lilei/whatsapp/atlas/AtlasActivity.java
@@ -39,9 +39,12 @@ public class AtlasActivity extends Activity {
                 .pkgName("com.netease.newsreader.activity").apkUrl("plugin_02.apk").build();
         AppObject appObject03 = new AppObject.Builder().id("0x0001").name("Season 1").iconUrl("http://10.4.232.53:9528/plugin/plugin_03.png")
                 .pkgName("com.leili.season1").apkUrl("plugin_03.apk").build();
+        AppObject appObject04 = new AppObject.Builder().id("0x0001").name("WeChat6.20").iconUrl("http://10.4.232.53:9528/plugin/plugin_04.png")
+                .pkgName("com.tencent.mm").apkUrl("plugin_04.apk").build();
         appList.add(appObject01);
         appList.add(appObject02);
         appList.add(appObject03);
+        appList.add(appObject04);
         appAdapter = new AppAdapter(this, appList);
     }
 
